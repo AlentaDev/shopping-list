0ad19622a8f4868aec68a99d1debfaeaee559ced
Juan Ginés <juangalenta@gmail.com>
2026-02-23 23:00:12 +0100

    refactor(api): make all new drafts autosave by default

--- apps/api/src/modules/lists/application/ReuseList.ts
+++ apps/api/src/modules/lists/application/ReuseList.ts
@@ -52,7 +52,7 @@ export class ReuseList {
       id: newListId,
       ownerUserId: list.ownerUserId,
       title: list.title,
-      isAutosaveDraft: existingDraft?.isAutosaveDraft ?? false,
+      isAutosaveDraft: true,
       status: "DRAFT",
       items: reusedItems,
       isEditing: false,
